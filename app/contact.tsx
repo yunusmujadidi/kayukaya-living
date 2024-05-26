a0005bd289e1cdff832cc9a6810b17134f19c5da
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-05-26 21:57:09 +0700

    chore: Add lazy loading for map component and update contact address

--- app/contact.tsx
+++ app/contact.tsx
@@ -13,12 +13,11 @@ export default function Contact() {
     <section className="justify-center flex flex-row items-center w-full py-12 md:py-24 lg:py-32 px-2 md:px-20 bg-[#2D2D2D] text-white">
       <div className="mb-8">
         <LazyMap />
+      </div>
+      <div>
         <h1 className="mt-4 text-2xl text-white text-center space-x-10">
           61 Ubi Ave 1, #06-10, UB Point, Singapore 408941{" "}
         </h1>
-      </div>
-      <div>
-        <CarouselContact />
       </div>
     </section>
   );
