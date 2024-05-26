c931ca1c93d221ae0f1b3478aecacf1000f56725
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-05-26 20:00:42 +0700

    Refactor import paths and update image URLs in Brands component

--- app/page.tsx
+++ app/page.tsx
@@ -12,18 +12,17 @@ import { useEffect, useRef, useState } from "react";
 import { useTransform, useScroll, motion, MotionValue } from "framer-motion";
 
 const images = [
-  "https://source.unsplash.com/random/?furniture?sig=1",
-  "https://source.unsplash.com/random/?furniture?sig=2",
-  "https://source.unsplash.com/random/?furniture?sig=3",
-  "https://source.unsplash.com/random/?furniture?sig=4",
-  "https://source.unsplash.com/random/?furniture?sig=5",
-  "https://source.unsplash.com/random/?furniture?sig=6",
-  "https://source.unsplash.com/random/?furniture?sig=7",
-  "https://source.unsplash.com/random/?furniture?sig=8",
-  "https://source.unsplash.com/random/?furniture?sig=9",
-  "https://source.unsplash.com/random/?furniture?sig=10",
-  "https://source.unsplash.com/random/?furniture?sig=11",
-  "https://source.unsplash.com/random/?furniture?sig=12",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1664010362273-MZ15A4OO1I3ZG2JTIQOK/IMG_3901.jpg",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1664118296020-GDTOAOND6FL4X0OCZ1GJ/IMG_8763.jpeg?format=2500w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1664117324539-S7HC5TBNQLB9XSOL1LYR/IMG_8037.jpg?format=2500w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1617847679395-SHF29I9LWUF0MJA2UYO6/WhatsApp+Image+2021-04-08+at+9.52.47+AM.jpeg?format=2500w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1664118829461-NUXVB3CD4XX9U0LMVGVF/maple+pews.jpg?format=2500w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1664114030064-9U0D7O3SZ6KP7U76Y2FA/3fb23ea1-3a03-4a62-b81d-99c79be49836.jpg?format=300w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1677589620343-RXFPRVKO4UTQUWMKUU6R/2cdf9186-b074-470c-be41-b228f5294e8d.jpg?format=1500w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1573459330387-TZ3JXR3O7CSBHS42L2G6/ray3.jpeg?format=2500w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1664451095051-XEIW4ST1Y4F8IYRVB6R9/IMG_3096.jpeg?format=2500w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1664450748653-KVUXVWD739GCYZ6CKTTO/alu+smart+capella+284668.jpg?format=2500w",
+  "https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1664013434480-N7J232LVDIW5M0Y67V23/2f569f62-4312-48bd-ae38-be477816e2c1.jpg?format=2500w",
 ];
 
 const Column = ({
