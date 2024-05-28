600a2a6daa2bced70fd8a2c2abf20ce3119217e4
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-05-28 07:45:08 +0700

    feat: Update page components and styles

--- app/factory/page.tsx
+++ app/factory/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import Title from "@/app/title";
 import Logo from "@/lib/logo";
 import Image from "next/image";
+import TextMotion from "@/components/ui/text-motion";
+import CustomFurniture from "../CustomFurniture";
 
 export default function Page({ params }: { params: any }) {
   return (
@@ -16,7 +18,12 @@ export default function Page({ params }: { params: any }) {
       >
         <h1 className="text-white text-4xl font-bold ">Factory</h1>
       </section>
-      <Title>Factory</Title>
+      <Title>Hotel Furniture Manufacturing Plant </Title>
+      <TextMotion
+        className="w-full py-10 px-2 md:px-20 text-xl"
+        paragraph="Our partner facility here in Indonesia has a state of the art facility Being committed to the improvement of furniture craftsmanship technology and quality, we have a large-scale technical research and development team with more than 400 professionals to cater to your brief from veneer to a dedicated CNC area with 10 machines, special finishes and custom area"
+      />
+      <CustomFurniture bgColor="bg-white" textColor="text-black" />
     </div>
   );
 }
