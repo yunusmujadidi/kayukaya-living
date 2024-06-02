2d3133cfe7dbe45e478d1f59cb36ad6c7d8a78f5
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-06-02 13:42:20 +0700

    style: Update about section content and formatting

--- app/aboutsection.tsx
+++ app/aboutsection.tsx
@@ -11,30 +11,33 @@ const AboutSection = () => {
               KayuKaya Living
             </h1>
             <p className="text-gray-300 text-lg leading-relaxed">
-              KayuKaya is a bespoke hospitality furniture solutions provider,
+              KayuKaya specializes in bespoke hospitality furniture solutions,
               catering to the commercial FF&E market with a focus on OEM or VE
-              requirements for both exterior and interior applications. The
-              company specializes in large-scale manufacturing with shorter lead
-              times and on-budget delivery, utilizing premium materials to meet
-              the evolving demands of the market.
+              requirements for both exterior and interior applications. Our
+              expertise lies in large-scale manufacturing with shorter lead
+              times and on-budget delivery. We utilize premium materials to meet
+              the evolving demands of the market, and our flexible approach
+              allows us to work closely with clients&apos; concepts, designs,
+              and briefs.
             </p>
             <p className="text-gray-300 text-lg leading-relaxed">
-              KayuKaya&apos;s flexible approach allows them to work closely with
-              clients&apos; concepts, designs, and briefs. The company has an
-              impressive portfolio of collaborations with high-end hotels,
-              resorts, F&B establishments, public spaces, and condominiums
-              worldwide, including the Ritz Carlton in the Maldives, The Patina
-              Maldives, The Royal Atlantis in Dubai, The Langham in Pasadena,
-              Four Seasons in Bora Bora, Shangri La in Hong Kong, and many
+              Our impressive portfolio includes collaborations with high-end
+              hotels, resorts, F&B establishments, public spaces, and
+              condominiums worldwide, such as the Ritz Carlton in the Maldives,
+              The Patina Maldives, The Royal Atlantis in Dubai, The Langham in
+              Pasadena, Four Seasons in Bora Bora, Shangri La, HK and many
               others across the globe, including Singapore.
             </p>
             <p className="text-gray-300 text-lg leading-relaxed">
-              In response to customer needs, KayuKaya expanded its offerings in
-              2008 to include professional shades, umbrellas, and selected
-              furniture.
+              In response to customer needs, we expanded our offerings in 2008
+              to include professional shades, umbrellas, and selected furniture
+              brands. As the Sole Authorized Agent for Glatz in Asia, we offer a
+              range of products, including umbrellas from Glatz Swiss (High wind
+              proof Umbrella) Jardinico Belgium (Resort type Umbrellas) , and
+              Coro Italia. (outdoor yacht furniture )
             </p>
           </div>
-          <div className="grid gap-6">
+          <div className="grid gap-6 items-center">
             <Image
               src="https://images.squarespace-cdn.com/content/v1/5dbfb66232cd095744bc5bdb/1585285634115-B6OEW1X268G7D221HK8V/Main+page+-+de+castelli_.jpg"
               alt="KayuKaya Living"
